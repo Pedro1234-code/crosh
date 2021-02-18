8d9fff01737ed00292077d214a0610a7c233afb8
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-02-18 18:41:21 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Terminal",
+   "homepage": "https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
